28dcc5ddb16daae29a09e2ca33ef743f2f845cb7
Riski Darmawan <46347378+FR13ND8@users.noreply.github.com>
2019-01-06 14:27:18 +0700

    Add files via upload

---
+++ TOOLSv2.sh
@@ -0,0 +1,822 @@
+#!/bin/bash
+
+#khusus mukak tamvan yg bisa makai tools ini
+#mukak buriq tidak boleh
+#jangan recode ya bangsat
+#orang yg recode gak bakalan punya skill
+#camkan itu sumanto >:|
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo "\033[34;1m"
+echo "\033[32;1m[\033[34;1m•\033[32;1m]\033[31;1m\033[35;1m[\033[32;1mJika gk tau username sama pasword nya    \033[35;1m]\033[31;1m\033[32;1m[\033[34;1m•\033[32;1m]"
+echo "\033[32;1m[\033[32;1m•\033[32;1m]\033[31;1m\033[35;1m[\033[32;1mKAMU BISA NANYAK DENGAN YG PUNYA TOOLS   \033[35;1m]\033[31;1m\033[32;1m[\033[34;1m•\033[32;1m]"
+echo "\033[32;1m[\033[32;1m•\033[32;1m]\033[31;1m\033[35;1m[\033[32;1mJANGAN DI RECODE YA BANGSAT CAPEK GUA ASW\033[35;1m]\033[31;1m\033[32;1m[\033[34;1m•\033[32;1m]"
+echo "     \033[32;1m[\033[34;1m•\033[32;1m]\033[31;1m\033[35;1m[ \033[34;1mKontak wa \033[32;1m: \033[31;1m085836465872 \033[35;1m]\033[31;1m\033[32;1m[\033[34;1m•\033[32;1m]"
+echo
+echo "\033[33;1m"
+read -p "           [•]---[ Username ]--[ : " user
+if [ $user = "Riski" ]
+then
+sleep 1
+else
+echo "      \033[32;1m[\033[31;1m!\033[32;1m]\033[34;1m----\033[35;1m[ \033[31;1mUsername Salah \033[35;1m]\033[34;1m----\033[32;1m[\033[31;1m!\033[32;1m]"
+sleep 1
+clear
+sh TOOLSv2.sh
+fi
+
+echo "\033[33;1m"
+read -p "           [•]---[ Pasword ]--[ : " pass
+if [ $pass = "Darmawan" ]
+then
+sleep 1
+clear
+else
+echo "      \033[32;1m[\033[31;1m!\033[32;1m]\033[34;1m----\033[35;1m[ \033[31;1mPassword Salah \033[35;1m]\033[34;1m----\033[32;1m[\033[31;1m!\033[32;1m]"
+sleep 1
+clear
+sh TOOLSv2.sh
+fi
+
+echo -n $cyan"Anda memakai tools ini pada tanggal "; date +"%d %B %Y"|lolcat
+echo "\033[36;1m[]=====================================================[]"|lolcat
+echo "\033[37;1m[] AUTHOR :  RISKI DARMAWAN                            []"|lolcat
+echo "\033[32;1m[] TEAM   : CYBER TEAM SECURITY INDONESIA              []"|lolcat
+echo "\033[35;1m[] W A    :   085836465872                             []"|lolcat
+echo "\033[36;1m[] NO HP  : 085835295133                               []"|lolcat
+echo "\033[31;1m[] PESAN  : GUNAKAN TOOLS INI DENGAN BIJAK             []"|lolcat
+echo "\033[36;1m[]=====================================================[]"|lolcat
+echo $cyan"[+]===================================================[+]"|lolcat
+echo $cyab"                 SELAMAT DATANG DI TOOLS SAYA"|lolcat
+echo $cyan"[+]===================================================[+]"|lolcat
+echo "                       ¥¥¥¥¥¥¥¥¥¥¥¥"|lolcat
+echo $cyan"[+]===================================================[+]"|lolcat
+echo $cyan"                   ^_^ Tools Gabungan ^_^"|lolcat
+echo $cyan"[+]===================================================[+]"|lolcat
+echo
+echo  "________________________________________________________" | lolcat
+echo
+echo $green"1) Toools tuan badutv1"|lolcat
+echo
+echo $cyan"2) Tools tuan badutv4"|lolcat
+echo
+echo $white"3) Tools B4j1n64nv5"|lolcat
+echo
+echo $blue"4) Tools B4j1n64nv6"|lolcat
+echo
+echo $purple"5) Hammer buat ddos"|lolcat
+echo
+echo $red"6) Lite -Ddos buat ddos"|lolcat
+echo
+echo $yellow"7) Xerxes buat ddos"|lolcat
+echo
+echo $green"8) Torhammer buat ddos"|lolcat
+echo
+echo $cyan"9) Bermain moon-buggy"|lolcat
+echo
+echo $white"10) Musikan di termux"|lolcat
+echo
+echo $blue"11) MALICIOUS BUAT VIRUS"|lolcat
+echo
+echo $purple"12) Instal OSIF"|lolcat
+echo
+echo $red"13) Hack fb mbf/massal"|lolcat
+echo
+echo $yellow"14) Hack cctv"|lolcat
+echo
+echo $green"15) Instal nethunter"|lolcat
+echo
+echo $cyan"16) Spammer sms"|lolcat
+echo
+echo $white"17) hacker Recondog"|lolcat
+echo
+echo $blue"18) Instal Sungai atau kali linux no root"|lolcat
+echo
+echo $purple"19) Deface"|lolcat
+echo
+echo $red"20) Pulsa Gratis all operator"|lolcat
+echo
+echo $blue"21) Tools mr.cakil"|lolcat
+echo
+echo $purple"22) ko-dork"|lolcat
+echo
+echo $red"23) buat script deface"|lolcat
+echo
+echo $white"24) Sadap whatsapp jarak jauh"|lolcat
+echo
+echo $blue"25) Tools Moreno77"|lolcat
+echo
+echo $green"26) weeman"|lolcat
+echo
+echo $purple"27) PhisingGame"|lolcat
+echo
+echo $yellow"28) install hash buster"|lolcat
+echo
+echo $blue"29) Fb-React"|lolcat
+echo
+echo $white"30) Hack fb narget"|lolcat
+echo
+echo $red"31) instal sqlmap"|lolcat
+echo
+echo $green"32) install santet online buat santet mantan"|lolcat
+echo
+echo $purple"33) install metasploit"|lolcat
+echo
+echo $blue"34) install infoga"|lolcat
+echo
+echo $red"35) install instabot"|lolcat
+echo
+echo $purple"36) Hack Life Facebook"|lolcat
+echo
+echo $blue"37) install breacher"|lolcat
+echo
+echo $white"38) Install RouterSploit"|lolcat
+echo
+echo $red"39) Install D-TECT"|lolcat
+echo
+echo $red"40) Menampilkan Teks ascii"|lolcat
+echo
+echo $red"41) Bot fb"|lolcat
+echo
+echo $red"42) Download Apk Hack Facebook"|lolcat
+echo
+echo $red"43) DEFACE VERSI R15K1"|lolcat
+echo
+echo $red"44) Pulsa Gratis All Operator"|lolcat
+echo
+echo $red"45) Install Katoolin"|lolcat
+echo
+echo $red"46) Auto Create Facebook"|lolcat
+echo
+echo $red"47) install Gps tracking"|lolcat
+echo
+echo $red"48) install Sn1Per"|lolcat
+echo
+echo $red"49) install wpbf"|lolcat
+echo
+echo $yellow"50) Keluar"|lolcat
+echo  "________________________________________________________" | lolcat
+echo
+echo "\033[32;1m╭──=>>[PILIH NOMORNYA]"
+read -p "╰──────────────────────────> : " bos
+
+if [ $bos = 1 ] || [ $bos = 1 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pjg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+chmod +x Tuanb4dut.sh
+sh Tuanb4dut.sh
+fi
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+ls
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $bos = 3 ] || [ $bos = 3 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install git
+pkg insatll toilet
+pkg install figlet
+pip2 install lolcat
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $bos = 4 ] || [ $bos = 4 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+ls
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $bos = 5 ] || [ $bos = 5 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $bos = 6 ] || [ $bos = 6 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $bos = 7 ] || [ $bos = 7 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+clear
+apt update
+apt install git
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+read -p "Masukan Target==>:" target
+python2 torshammer.py -T -t target
+fi
+
+if [ $bos = 8 ] || [ $bos = 8 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt install git
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+mv xerxes $HOME
+cd $HOME/xerxes
+clang xerxes.c -o xerxes
+clear
+read -p "[masukanWebsiteTarget]>" target
+./xerxes target 80
+fi
+
+if [ $bos = 9 ] || [ $bos = 9 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $bos = 10 ] || [ $bos = 10 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install mpv
+echo $cyan"mpv/sdcard/lagu.mp3 /sdcard/"
+echo $red"Bisa ganti sesuai letak"
+fi
+
+if [ $bos = 11 ] || [ $bos = 11 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update
+pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/Hider5/Malicious
+cd Malicious
+pip2 install -r requirements.txt
+chmod 777 malicious.py
+python2 malicious.py
+fi
+
+if [ $bos = 12 ] || [ $bos = 12 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install bash
+pkg install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $bos = 13 ] || [ $bos = 13 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update
+apt upgrade
+apt install python2
+apt install git
+pip2 install --upgrade pip
+pip2 install mechanize
+git clone https://github.com/MRA27/mbf
+cd mbf
+python2 mbf.py
+fi
+
+if [ $bos = 14 ] || [ $bos = 14 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python2
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $bos = 15 ] || [ $bos = 15 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update
+apt install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+cd Nethunter
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+fi
+
+if [ $bos = 16 ] || [ $bos = 16 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install git
+pkg install python2
+pip2 install requests
+git clone https://github.com/Kizukaa/SpammerSMS
+cd SpammerSMS
+fi
+
+if [ $bos = 17 ] || [ $bos = 17 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update
+apt install python python2
+apt install git
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if [ $bos = 18 ] || [ $bos = 18 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+clear
+pkg update
+pkg upgrade
+clear
+pkg install git 
+clear
+pkg install python2 
+clear 
+pkg install gnupg
+clear 
+Pkg install nano 
+clear
+git clone https://github.com/LionSec/katoolin
+cd katoolin
+python2 katoolin.py
+fi
+
+if [ $bos = 19 ] || [ $bos = 19 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/21D4N404/Defacer
+cd Defacer
+chmod 777 ICA.sh
+sh ICA.sh
+fi
+
+if [ $bos = 20 ] || [ $bos = 20 ]
+then
+clear
+figlet T E R T I P U
+figlet B E L I K  L A H
+figlet G O B L O K
+exit
+fi
+
+if [ $bos = 21 ] || [ $bos = 21 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python2
+pkg install
+git clone https://github.com/mrcakil/Mrcaki
+cd Mrcaki
+chmod 777 tools
+./tools
+fi
+
+if [ $bos = 22 ] || [ $bos = 22 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update && pkg upgrade
+pkg install git python2
+git clone https://github.com/ciku370/ko-dork
+cd ko-dork
+python2 dork.py
+fi
+
+if [ $bos = 23 ] || [ $bos = 23 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python2
+pkg install php
+pkg install openssl curl
+pkg install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $bos = 24 ] || [ $bos = 24 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python2
+pkg install nano
+pkg install php
+pkg install git
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+sh whatshack.sh
+fi
+
+if [ $bos = 25 ] || [ $bos = 25 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+git clone https://github.com/DarkCurut08/DarkCurut08
+cd DarkCurut08
+sh Moreno77.sh
+fi
+
+if [ $bos = 26 ] || [ $bos = 26 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt-get update
+apt-get upgrade
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $bos = 27 ] || [ $bos = 27 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade
+git clone https://github.com/Senitopeng/PhisingGame
+cd PhisingGame
+python2 phising.py
+fi
+
+if [ $bos = 28 ] || [ $bos = 28 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade -y
+apt install python2 git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+echo "Cara menjalankan hash buster tinggal ketik cd Hash-Buster terus python2 hash.py" | lolcat
+fi
+
+if [ $bos = 29 ] || [ $bos = 29 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade
+apt install php curl -y
+git clone http://github.com/AMVengeance/FB-React.git
+cd FB-React
+chmod +x start
+./start
+fi
+
+if [ $bos = 30 ] || [ $bos = 30 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update && pkg upgrade
+pkg install git unzip figlet
+pkg intall python2
+pip2 install mechanize
+pip2 install requests
+git clone https://gitlab.com/AlatToll/brute.git
+cd brute
+unzip brute
+bash brute
+fi
+
+if [ $bos = 31 ] || [ $bos = 31 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update
+apt upgrade
+apt install python
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $bos = 32 ] || [ $bos = 32 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update
+apt upgrade
+pkg install python
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python santet.py
+fi
+
+if [ $bos = 33 ] || [ $bos = 33 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update
+pkg upgrade
+pkg install wget
+wget https://Auxilus.github.io/metasploit.sh
+sh metasploit.sh
+cd metasploit-framework
+./msfconsole
+fi
+
+if [ $bos = 34 ] || [ $bos = 34 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update
+apt upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/m4ll0k/infoga
+cd infoga
+python2 infoga.py
+fi
+
+if [ $bos = 35 ] || [ $bos = 35 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade
+apt install python2 -y
+apt install git -y
+git clone https://github.com/instagrambot/instabot
+pip2 install -U instabot
+cd instabot
+cd examples
+cd Ultimate
+python2 ultimate.py
+fi
+
+if [ $bos = 36 ] || [ $bos = 36 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update
+pkg upgrade
+git cloe https://github.com/silentghostaka/livehackfb.git
+cd livehackfb
+pip3 install requests bs4
+pip install mechanize
+python3 fb.py
+fi
+
+if [ $bos = 37 ] || [ $bos = 37 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/UltimateHackers/Breacher
+cd Breacher
+python2 breacher.py
+fi
+
+if [ $bos = 38 ] || [ $bos = 38 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade
+apt install git
+apt install python
+apt install python2 
+pip2 install requests
+git clone https://github.com/reverse-shell/routersploit.git
+cd routersploit
+pip install -r requirements.txt termux-fix-shebang rsf.py
+cd routersploit
+./rsf.py
+fi
+
+if [ $bos = 39 ] || [ $bos = 39 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+apt update && apt upgrade
+pkg install python2 
+pkg install git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+chmod +x d-tect.py
+python2 d-tect.py
+fi
+
+if [ $bos = 40 ]
+then
+clear
+pkg install figlet
+clear
+figlet -f slant Riski |lolcat
+figlet -f slant Ganteng |lolcat
+fi
+
+if [ $bos = 41 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+apt update
+apt upgrade
+pkg install git
+pkg install python2
+pip2 install --upgrade pip
+pip2 install mechanize
+git clone https://github.com/LOolzeC/BlackEngineer
+cd BlackEngineer
+python2 BlackEngineer.py
+fi
+
+if [ $bos = 42 ]
+then
+clear
+echo $red"Copas Link Di Bawah Ini Lalu Tempelkan Ke Google"|lolcat
+echo $red"<×>=================================================<×>"|lolcat
+echo
+echo $red"http://www.mediafire.com/file/5vsqbabm52x8as5/Hack_Fb.apk/file"|lolcat
+echo
+echo $red"<×>=================================================<×>"|lolcat
+fi
+
+if [ $bos = 43 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+pkg install figlet
+pkg install lolcat
+gem install lolcat
+git clone https://github.com/FR13ND8/Deface
+cd Deface
+python2 ganss.py
+fi
+
+if [ $bos = 44 ]
+then
+clear
+figlet -f slant "3"|lolcat
+figlet -f slant "2"|lolcat
+figlet -f slant "1"|lolcat
+figlet -f slant TERTIPU |lolcat
+figlet -f slant BELIK LAH |lolcat
+figlet -f slant KONTOL |lolcat
+fi
+
+if [ $bos = 45 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+apt update
+apt upgrade
+pkg install git
+pkg install python2
+pkg install nano
+pkg install gnupg
+git clone https://github.com/LionSec/katoolin.git
+cd katoolin
+python2 katoolin.py
+nano katoolin.py
+fi
+
+if [ $bos = 46 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+pkg update
+pkg upgrade
+pkg install git
+pkg install python2
+pip2 install bs4 mechanize requests
+git clone https://github.com/zevtyardt/fb-maker
+cd fb-maker
+pip2 install -r requirements.txt
+python2 fb-maker.py
+python2 fb-maker.py
+fi
+
+if [ $bos = 47 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+pkg update
+pkg upgrade
+pkg install php -y
+pkg install git -y
+git clone https://github.com/indosecid/gps_tracking
+cd gps_tracking
+php gps.php
+fi
+
+if [ $bos = 48 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+git clone https://github.com/1N3/Sn1per
+cd Sn1per
+chmod +x install.sh
+./install.sh
+fi
+
+if [ $bos = 49 ]
+then
+clear
+figlet -f slant "Loading.."|lolcat
+apt-get update
+apt-get upgrade
+apt-get install python
+apt-get install python-pip
+pip install request
+git clone https://github.com/atarantini/wpbf
+cd wpbf
+python2 wpbf.py
+fi
+
+if [ $bos = 50 ] || [ $bos = 50 ]
+then
+clear
+   echo "\033[35;1mSemoga bermanpaat dan gunakan dengan bijak :)"|lolcat
+   echo
+   echo
+   sleep 1
+   echo "\033[32;1mJika ingin kembali lagi silahkan ketik sh TOOLSv2.sh"|lolcat
+   echo
+   echo
+   sleep 1
+   echo "\033[31;1mSalam Anak Medan"|lolcat
+   echo
+   echo
+   sleep 2
+   echo "\033[34;1mAnak Muda Berkarya Bukan Bergaya"|lolcat
+   echo
+   echo
+   sleep 3
+   echo "\033[36;1mWASSALAMU'ALAIKUM WR.WB..." | lolcat
+   sleep 1
+   
+  exit
+  
+ fi
+    
+    
+exit
